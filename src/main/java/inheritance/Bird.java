6019805786d8bcb9ddbf6086817da50a31c0e727
kdgitcollect <zaz01008@gmail.com>
2018-04-02 23:00:50 +0530

    inheritance files

---
+++ src/main/java/inheritance/Bird.java
@@ -0,0 +1,16 @@
+package main.java.inheritance;
+
+public class Bird extends Animal {
+	public Bird() {
+		super();
+		System.out.println("A new bird has been created!");
+	}
+	
+	public void sleep() {
+		System.out.println("A bird sleeps...");
+	}
+	
+	public void eat() {
+		System.out.println("A bird eats...");
+	}
+}
