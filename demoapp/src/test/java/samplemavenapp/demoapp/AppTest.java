3281fdd8ff8068b8bcb1478840a4eac6b4226d1b
kdgitcollect <zaz01008@gmail.com>
2018-05-14 06:56:40 +0530

    change for development2

--- demoapp/src/test/java/samplemavenapp/demoapp/AppTest.java
+++ demoapp/src/test/java/samplemavenapp/demoapp/AppTest.java
@@ -16,7 +16,10 @@ public class AppTest
      * @param testName name of the test case
      */
     public AppTest( String testName )
+
     {
+
+    	System.out.println("added for development branch");
         super( testName );
     }
 
